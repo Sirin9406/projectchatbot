c35a785410c535db462f55a598e63d0f97c64c59
Sirin9406 <fonsirinapa94@gmail.com>
2022-04-03 03:46:07 +0700

    edit les menu

--- src/Lesson.tsx
+++ src/Lesson.tsx
@@ -5,46 +5,46 @@ import "./css/Lesson.css";
 export const Lesson: FunctionComponent = () => {
   const navigate = useNavigate();
 
-  const onGroupButton1Click = useCallback(() => {
+  const onGroupButtonClick = useCallback(() => {
     navigate("/lesson1");
   }, [navigate]);
 
   return (
     <div className="lesson-div">
-      <button className="group-button" autoFocus>
-        <img className="group-icon" alt="" src="group-5.svg" />
-        <b className="b">+</b>
-      </button>
-      <button className="group-button1" autoFocus onClick={onGroupButton1Click}>
+      <img className="bg-lesson-icon" alt="" src="bg-lesson@2x.png" />
+      <img
+        className="download-premium-png-of-purple"
+        alt=""
+        src="download-premium-png-of-purple-fluid-background-frame-design-element--by-wifiseeker-about-business-frame-fluid-corporate-and-fluid-background-2340000-1@2x.png"
+      />
+      <button className="group-button" autoFocus onClick={onGroupButtonClick}>
         <div className="rectangle-div" />
         <div className="rectangle-div1" />
-        <b className="basic-command">Basic command</b>
+        <b className="basic">Basic</b>
+        <img className="pixels-1-icon" alt="" src="pixels-1@2x.png" />
       </button>
       <div className="group-div">
         <div className="rectangle-div" />
         <div className="rectangle-div1" />
-        <b className="lesson-2-b">lesson 2</b>
+        <b className="lesson-2-b">Input</b>
+        <img className="enter-1-icon" alt="" src="enter-1@2x.png" />
       </div>
       <div className="group-div1">
         <div className="rectangle-div" />
         <div className="rectangle-div1" />
-        <b className="lesson-2-b">lesson 3</b>
+        <b className="music-b">Music</b>
+        <img className="music-1-icon" alt="" src="music-1@2x.png" />
       </div>
       <div className="group-div2">
         <div className="rectangle-div" />
         <div className="rectangle-div1" />
-        <b className="lesson-2-b">lesson 4</b>
+        <b className="lesson-4-b">lesson 4</b>
       </div>
-      <div className="group-div3">
-        <div className="rectangle-div" />
-        <div className="rectangle-div1" />
-        <b className="lesson-2-b">lesson 5</b>
-      </div>
-      <div className="group-div4">
-        <div className="rectangle-div" />
-        <div className="rectangle-div1" />
-        <b className="lesson-2-b">lesson 6</b>
-      </div>
+      <img className="polygon-icon" alt="" src="polygon-1.svg" />
+      <img className="polygon-icon1" alt="" src="polygon-2.svg" />
+      <img className="polygon-icon2" alt="" src="polygon-3.svg" />
+      <img className="polygon-icon3" alt="" src="polygon-4.svg" />
+      <img className="previous-2-icon" alt="" src="previous-2@2x.png" />
     </div>
   );
 };
